9b83e29d24b29bb80dcacdde558e459ed134c3f9
rs98065 <rahul.sharma2@kroger.com>
2021-10-01 16:25:44 -0700

    new release v2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,10 +1,13 @@
 package calc
 
-func Add(numbers ... int) int {
+func Add(numbers ... int) (error,int) {
+	
+	if len(numbers < 2)
+		return errors.New("Need more than 2 numbers"),0
 	sum := 0
 
 	for _, num := range numbers {
 		sum = sum + num
 	}
-	return sum
+	return nil,sum
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rssharma75/go-nummanip/v2
+
+go 1.16
